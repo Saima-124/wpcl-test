bc44ad0f05ecc420c6ab08a2c9b6a4d875b77935
Saima <mstsaima26072@gmail.com>
2025-02-25 02:27:27 +0600

    . Eliminates the ‘done’ Control-Flow Variable. Adds Better Response for Search Operation

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -44,11 +44,13 @@ public class EmployeeManager {
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
-                for (String employee : readEmployeesFromFile()) {
-                    if (employee.equals(args[0].substring(1))) {
-                        System.out.println("Employee found!");
-                        break;
-                    }
+                String[] employees = readEmployeesFromFile();
+                String searchEmployee = args[0].substring(1);
+                boolean found = Arrays.asList(employees).contains(searchEmployee);
+                if (found) {
+                    System.out.println("Employee found!");
+                } else {
+                    System.out.println("Employee not found.");
                 }
             } catch (Exception e) {
 
@@ -92,7 +94,6 @@ public class EmployeeManager {
             System.out.println("Data Deleted.");
         }
     }
-
 }
 
 private static String[] readEmployeesFromFile() throws IOException {
